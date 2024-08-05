a4814daa8a52f238894c911cafa26bbc2ac92ad1
shaunpetro <shaun.p.mckoy@gmail.com>
2024-08-05 20:16:10 +0200

    code fixes

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,10 +10,10 @@ def minOperations(n):
     returns:
         Integer : if n is impossible to achieve, return 0
     """
-    pasted_chars = 1 # how many chars in the file
-    clipboard = 0 # how many H's copied
-    counter = 0 # operations counter
-    
+    pasted_chars = 1  # how many chars in the file
+    clipboard = 0  # how many H's copied
+    counter = 0  # operations counter
+
     while pasted_chars < n:
         # if did not copy anything yet
         if clipboard == 0:
@@ -21,7 +21,7 @@ def minOperations(n):
             clipboard = pasted_chars
             # increment operations counter
             counter += 1
-            
+
         # if haven't pasted anything yet
         if pasted_chars == 1:
             # paste
@@ -30,8 +30,8 @@ def minOperations(n):
             counter += 1
             # continue to next loop
             continue
-        
-        remaining = n - pasted_chars # remaining chars to paste
+
+        remaining = n - pasted_chars  # remaining chars to paste
         # check if impossible by checking if clipboard
         # has more than needed to reach the desired no
         # which also means no of chars in file =
@@ -39,7 +39,7 @@ def minOperations(n):
         # in both situations its impossible to achieve n chars
         if remaining < clipboard:
             return 0
-        
+
         # if can't be divided
         if remaining % pasted_chars != 0:
             # pasre current clipboard
@@ -53,7 +53,7 @@ def minOperations(n):
             pasted_chars += clipboard
             # increment operations counter
             counter += 2
-            
+
     # if got the desired result
     if pasted_chars == n:
         return counter
