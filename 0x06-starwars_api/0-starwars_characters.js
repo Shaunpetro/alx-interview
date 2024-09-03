42102fefb36ba87fc80e224784f942dce0ace6ff
Petro Malamule <133478698+Shaunpetro@users.noreply.github.com>
2024-09-03 18:28:56 +0200

    task file- p1

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const moveId = process.argv[2];
+const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + moveId;
+let people = [];
+const names = [];
+
+const requestCharacters = async () => {
+    await new Promise(request(filmEndPoint, (err, res, body) => {
+        if (err || res.statusCode !== 200) {
+            console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+        } else {
+            const jsonBody = JSON.parse(body);
+            people = jsonBody.characters;
+            resolve();
+        }
+    }));
+};
